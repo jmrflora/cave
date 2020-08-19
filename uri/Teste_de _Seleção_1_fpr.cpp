26f3f09882ba34849c598c30d040f5acdcd1c003
José Flora <jmrfloraxbox@gmail.com>
2020-08-19 14:09:16 -0300

    Create Teste_de _Seleção_1_fpr.cpp

---
+++ uri/Teste_de _Seleção_1_fpr.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+Leia 4 valores inteiros A, B, C e D.
+ A seguir, se B for maior do que C e se D for maior do que A,
+  e a soma de C com D for maior que a soma de A e B e se C e D,
+  ambos, forem positivos e se a variável A for par escrever a 
+  mensagem "Valores aceitos", senão escrever "Valores nao aceitos".
+*/
+int main() {
+ 
+ 	int A, B, C, D;
+ 	
+ 	scanf("%d %d %d %d", &A, &B, &C, &D);
+ 	
+ 	if((A%2 == 0) && (C>0 && D>0) && ((C + D) > (A + B)) && (D > A) && (B > C)){
+ 		printf("Valores aceitos\n");
+	 }
+	 else {
+	 	printf("Valores nao aceitos\n");
+	 }
+ 	
+	return 0;
+}
