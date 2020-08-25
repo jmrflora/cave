aebd63e4270b48763d618af5baeae8969a7bcc33
José Flora <jmrfloraxbox@gmail.com>
2020-08-25 09:00:52 -0300

    Create sort_simples.cpp

---
+++ uri/sort_simples.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+Leia 3 valores inteiros e ordene-os em ordem crescente.
+
+No final, mostre os valores em ordem crescente, uma linha em branco e em seguida, os valores na sequência como foram lidos.
+*/
+int main (){
+	
+	return 0;
+}
