9ef95e0a62cdf78c48ee9aa613d4edd915d0a5e3
José Flora <jmrfloraxbox@gmail.com>
2020-08-25 09:00:18 -0300

    Create Coordenadas_de_um_Ponto.cpp

---
+++ uri/Coordenadas_de_um_Ponto.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+/*
+Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano.
+
+A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+
+Se o ponto estiver na origem, escreva a mensagem “Origem”.
+
+Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+
+q1-> x >0 e y > 0
+
+q2-> x < 0 e y > 0
+
+q3-> x < 0 e y < 0
+
+q4-> x > 0 e y < 0
+*/
+int main(){
+	float x, y;
+	
+	scanf("%f %f", &x, &y);
+	
+	if(x == 0 && y == 0){
+		printf("Origem\n");
+	}
+	else{
+		if(x == 0.0 || y == 0.0){
+			if(x == 0){
+				printf("Eixo Y\n");
+			}
+			else{
+				printf("Eixo X\n");
+			}
+		}
+		else{
+			if(x > 0.0){
+				if(y > 0.0){
+					printf("Q1\n");
+				}
+				else{
+					printf("Q4\n");
+				}
+			}
+			else if(y > 0.0){
+				printf("Q2\n");
+			}
+			else{
+				printf("Q3\n");
+			}
+		}
+	}
+	return 0;
+}
+
