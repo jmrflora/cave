156c1117ea50d1c2c2b055f29c7419859748a119
José Flora <jmrfloraxbox@gmail.com>
2020-10-14 22:41:17 -0300

    Create teste_tela.cpp

---
+++ av1_fpr/teste_tela.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	float a, b, c, d, e, f;
+	a=10;
+	b=1.5;
+	c=12.5;
+	d=1;
+	e=2;
+	f=3;
+	/*
+	printf("          Notas originais      ");printf("  |");printf("          Notas com peso       ");printf("  |\n");
+	printf("_________________________________|");printf("_________________________________|");printf("_________________________________\n");
+	printf(" codigos  mat   lin   nat   red  |");		    printf("                                 |\n");
+	
+	printf("_________________________________|");printf("_________________________________|");printf("_________________________________\n");			  
+	
+	printf(" %7.2f%7.2f%7.2f%7.2f%7.2f", a, b, c,d,e);printf("  |");
+	*/
+	printf("               Notas originais             ");printf("  |");printf("          Notas com peso       ");printf("  |\n");
+	printf("_____________________________________________|");printf("_________________________________|");printf("_________________________________\n");
+	printf("  codigos   mat   lin     hum    nat    red  |");printf("   mat   lin   hum   nat   red   |");printf("  Media_P Situacao  \n");
+	printf("_____________________________________________|");printf("_________________________________|");printf("_________________________________\n");;
+															       
+		
+	printf(" %7.2f%7.2f%7.2f%7.2f%7.2f%7.2f", a=1+2, b, c,d,e,f);printf("  |");printf(" %6.2f%6.2f%6.2f%6.2f%6.2f", a, b, c,d,e);printf("  |");
+	
+	return 0;
+}
+
